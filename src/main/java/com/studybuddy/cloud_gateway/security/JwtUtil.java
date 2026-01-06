dddee7ed037a1936501fa71be7cb037bc4c0ca65
Namit-A-R <nandunamit2332@gmail.com>
2026-01-06 18:55:52 +0530

    jwt

---
+++ src/main/java/com/studybuddy/cloud_gateway/security/JwtUtil.java
@@ -0,0 +1,23 @@
+package com.studybuddy.cloud_gateway.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+
+    @Value("${app.jwtSecret}")
+    private String jwtSecret;
+
+    public Claims validateAndGetClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret)))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
